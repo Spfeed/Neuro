0d12177e791fd246d134325819051ff61ef4f72c
Spfeed <subbart2266@gmail.com>
2023-10-10 13:54:06 +0400

    1 лаба по нейронкам

--- main.py
+++ main.py
@@ -1,44 +1,46 @@
 import pandas as pd
 import numpy as np
+import sklearn
+from sklearn.tree import DecisionTreeClassifier
 
+#1 задание
 titanic=pd.read_csv(filepath_or_buffer="datasets/titanic.csv",
             sep=',')
-male=len(titanic[titanic['Sex']=='male'])
-female=len(titanic[titanic['Sex']=='female'])
+#5 задние - сразу для удобства исключим пустые значения
+titanic=titanic.dropna()
 
-print('man: ', male,',' ' woman: ', female)
+#2 задание
+tit_2=titanic[['Pclass','Fare','Age','Sex']].copy()
 
-passengerscount=len(titanic['PassengerId'])
+#3  задание
+tit_2['Sex']=tit_2['Sex'].replace({'female': 0, 'male': 1})
 
-survived=len(titanic[titanic['Survived']==1])
-survivors=round((survived/passengerscount)*100,2)
-print('passengers survived in percentage: ', survivors)
+#4 задание
+aim_var=titanic['Survived']
 
-firstclass=round((len(titanic[titanic['Pclass']==1])/passengerscount)*100,2)
-print('passengers of 1 class: ', firstclass)
+#6 задание
+clf=DecisionTreeClassifier(random_state=241)
+clf.fit(tit_2,aim_var)
 
-mediumage=titanic['Age'].sum()/passengerscount
-median=titanic['Age'].median()
-print('medium age: ',mediumage, ', median: ',median)
+#7 задание
+importances=clf.feature_importances_
+feature_importance_dict = dict(zip(tit_2.columns, importances))
+sorted_feature_importance = sorted(feature_importance_dict.items(), key=lambda x: x[1], reverse=True)
+top_two_features = sorted_feature_importance[:2]
+print("Два признака с наибольшей важностью:")
+for feature, importance in top_two_features:
+    print(f"{feature}: {importance}")
 
-correl=titanic['SibSp'].corr(titanic['Parch'])
-print('correlation: ',correl)
-
-test=titanic[titanic['Sex']=='female']
-
-def extract_female_name(name):
-    if '(' in name and ')' in name:
-
-        return name.split('(')[1].split(')')[0]
-    elif 'Miss.' in name:
-
-        return name.split('Miss. ')[1].split(' ')[0]
-    else:
-
-        return None
-
-test['FemaleName']=test['Name'].apply(extract_female_name)
-name_counts=test['FemaleName'].value_counts()
-name_counts=name_counts.dropna()
-most_popular_name=name_counts.idxmax()
-print(most_popular_name)
+#8 задание
+person_data={
+    'Pclass': 1,
+    'Fare': 27.7208,
+    'Age': 39,#3 не выживет((((
+    'Sex': 0
+}
+person_df=pd.DataFrame([person_data])
+survival_prediction=clf.predict(person_df)
+if survival_prediction[0]==0:
+    print("Человек не выжил")
+else:
+    print("Человек выжил")
