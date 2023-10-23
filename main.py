0d46513643dc32fc42e68dcac178be85725776de
Spfeed <subbart2266@gmail.com>
2023-10-23 23:24:07 +0400

    Лаба 5.1

--- main.py
+++ main.py
@@ -1,36 +1,27 @@
 import pandas as pd
-import numpy as np
+from sklearn.svm import SVC
 import matplotlib.pyplot as plt
-from sklearn.linear_model import Perceptron
-from sklearn.model_selection import train_test_split
 
-data = pd.read_csv('datasets/apples_pears.csv')
+# 1. Загрузка данных из CSV-файла
+data = pd.read_csv('datasets/svm-data.csv')
 
-# Визуализация данных
-plt.figure(figsize=(10, 8))
-plt.scatter(data.iloc[:, 0], data.iloc[:, 1], c=data['target'], cmap='rainbow')
-plt.title('Яблоки и груши', fontsize=15)
-plt.xlabel('симметричность', fontsize=14)
-plt.ylabel('желтизна', fontsize=14)
+# Выделение признаков (X) и целевой переменной (y)
+X = data.iloc[:, 1:]
+y = data.iloc[:, 0]
+
+# 2. Построение изображения набора данных в виде точек на плоскости
+plt.scatter(X.iloc[:, 0], X.iloc[:, 1], c=y, cmap='spring')
 plt.show()
 
-#Выделение матрицы признаков (X) и вектора ответов (y)
-X = data[['symmetry', 'yellowness']]
-y = data['target']
+# 3. Обучение классификатора с линейным ядром и параметром C = 100000
+svm_classifier = SVC(C=100000, kernel='linear', random_state=241)
+svm_classifier.fit(X, y)
 
-#Создание экземпляра перцептрона
-perceptron = Perceptron(random_state=241)
+# 4. Поиск номеров объектов, которые являются опорными
+support_vector_indices = svm_classifier.support_
+print("Номера опорных объектов:", support_vector_indices)
 
-#Обучение перцептрона
-perceptron.fit(X, y)
-
-#Получение предсказаний перцептрона
-y_pred = perceptron.predict(X)
-
-#Построение изображения набора данных "Яблоки-Груши" с учетом результатов классификации
-plt.figure(figsize=(10, 8))
-plt.scatter(data.iloc[:,0], data.iloc[:,1], c=y_pred, cmap='spring')
-plt.title('Яблоки и груши', fontsize=15)
-plt.xlabel('симметричность', fontsize=14)
-plt.ylabel('желтизна', fontsize=14)
-plt.show()
+# 5. Обученная модель для предсказания класса новой точки
+new_point = [[0.8, -0.6]]  # Пример новой точки с двумя числовыми характеристиками
+predicted_class = svm_classifier.predict(new_point)
+print("Предсказанный класс новой точки:", predicted_class)
