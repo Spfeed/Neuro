e56f30ad68ddba1647ea0059727940aca64c2898
Spfeed <subbart2266@gmail.com>
2023-10-30 09:22:10 +0400

    Лаба 9 вроде все работает

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-N = 100 #Количество точек
-D = 2#Количество признаков
-K = 3#Количество классов
+N = 100
+D = 2
+K = 3
 X = np.zeros((N * K, D))
 y = np.zeros(N * K, dtype='uint8')
 for j in range(K):
@@ -21,47 +21,97 @@ plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.rainbow)
 plt.title('Спираль', fontsize=15)
 plt.xlabel('$x$', fontsize=14)
 plt.ylabel('$y$', fontsize=14)
-plt.show()
+plt.show();
 
 X = torch.autograd.Variable(torch.FloatTensor(X))
 y = torch.autograd.Variable(torch.LongTensor(y.astype(np.int64)))
-print(X.data.shape, y.data.shape)
 
-# N - размер батча (batch_size, нужно для метода оптимизации)
-# D_in - размерность входа (количество признаков у объекта)
-# D_out - размерность выходного слоя (суть -- количество классов)
-N, D_in, D_out = 64, 2, 3
-neuron = torch.nn.Sequential(
-torch.nn.Linear(D_in, D_out),
+# N - размер батча (batch_size, нужно для метода оптимизации);
+# D_in - размерность входа (количество признаков у объекта);
+# H - размерность скрытых слоёв;
+# D_out - размерность выходного слоя (суть - количество классов)
+N, D_in, H, D_out = 64, 2, 100, 3
+# Use the nn package to define our model and loss function.
+two_layer_net = torch.nn.Sequential(
+torch.nn.Linear(D_in, H),
+torch.nn.ReLU(),
+torch.nn.Linear(H, D_out),
 )
 loss_fn = torch.nn.CrossEntropyLoss(reduction='none')
 learning_rate = 1e-4
-optimizer = torch.optim.SGD(neuron.parameters(), lr=learning_rate)
+optimizer = torch.optim.SGD(two_layer_net.parameters(), lr=learning_rate)
 
 for t in range(500):
     # forward
-    y_pred = neuron(X)
+    y_pred = two_layer_net(X)
     # loss
     loss = loss_fn(y_pred, y).sum()
     print('{} {}'.format(t, loss.data))
-    # зануляем градиенты с предыдущего шага
+    # зануляем градиенты (чтобы не было остатка с предыдущего шага)
     optimizer.zero_grad()
     # backward
     loss.backward()
-    # обновляем веса
-
+    # обновляем
     optimizer.step()
 
-# Обратно в Numpy для отрисовки
-X = X.data.numpy()
-y = y.data.numpy()
+
 h = 0.02
 x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
 y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
 np.arange(y_min, y_max, h))
 grid_tensor = torch.FloatTensor(np.c_[xx.ravel(), yy.ravel()])
-Z = neuron(torch.autograd.Variable(grid_tensor))
+Z = two_layer_net(torch.autograd.Variable(grid_tensor))
+Z = Z.data.numpy()
+Z = np.argmax(Z, axis=1)
+Z = Z.reshape(xx.shape)
+plt.figure(figsize=(10, 8))
+plt.contourf(xx, yy, Z, cmap=plt.cm.rainbow, alpha=0.3)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.rainbow)
+plt.xlim(xx.min(), xx.max())
+plt.ylim(yy.min(), yy.max())
+
+plt.title('Спираль', fontsize=15)
+plt.xlabel('$x$', fontsize=14)
+plt.ylabel('$y$', fontsize=14)
+plt.show();
+
+# X = torch.autograd.Variable(torch.FloatTensor(X))
+# y = torch.autograd.Variable(torch.LongTensor(y.astype(np.int64)))
+
+N, D_in, H, D_out = 64, 2, 100, 3
+better_net = torch.nn.Sequential(
+torch.nn.Linear(D_in, H),
+torch.nn.ReLU(),
+torch.nn.Linear(H, H),
+torch.nn.ReLU(),
+torch.nn.Linear(H, H),
+torch.nn.ReLU(),
+torch.nn.Linear(H, D_out),
+)
+loss_fn = torch.nn.CrossEntropyLoss(reduction='none')
+learning_rate = 1e-3
+optimizer = torch.optim.SGD(better_net.parameters(), lr=learning_rate)
+for t in range(1000):
+    # forward
+    y_pred = better_net(X)
+    # loss
+    loss = loss_fn(y_pred, y).sum()
+    print('{} {}'.format(t, loss.data))
+    # зануляем градиенты (чтобы не было остатка с редыдущего шага)
+    optimizer.zero_grad()
+    # backward
+    loss.backward()
+    # обновляем
+    optimizer.step()
+
+h = 0.02
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+np.arange(y_min, y_max, h))
+grid_tensor = torch.FloatTensor(np.c_[xx.ravel(), yy.ravel()])
+Z = better_net(torch.autograd.Variable(grid_tensor))
 Z = Z.data.numpy()
 Z = np.argmax(Z, axis=1)
 Z = Z.reshape(xx.shape)
