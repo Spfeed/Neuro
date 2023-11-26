9e60503ad6b08d576ee10c714cc03cc51c4ba968
Spfeed <subbart2266@gmail.com>
2023-11-26 12:49:16 +0500

    Лаба 11.1

--- main.py
+++ main.py
@@ -1,90 +1,125 @@
-import pixels2svg
-from PIL import Image
-from math import floor, ceil
-import matplotlib.pyplot as plt
+import torch
+import torchvision
+from torchvision import transforms
 import numpy as np
-import time
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.nn.functional as F # Functional
+from tqdm import tqdm
 
-from scipy.signal import convolve2d
+transform = transforms.Compose(
+[transforms.ToTensor()])
 
+trainset = torchvision.datasets.MNIST(root='./data', train=True,
+download=True, transform=transform)
 
-def convolution(a, b):
-    s = 0
-    for i in range(len(a)):
-        for j in range(len(a[0])):
-            s += a[i][j] * b[i][j]
-    return s
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+shuffle=True)
 
-filter = [
-    [-1, -1, 0, 0, 0],
-    [0, -1, -1, -1, 0],
-    [0, -1, 9, -1, 0],
-    [0, -1, -1, -1, 0],
-    [0, 0, 0, 0, 0]
-]
+testset = torchvision.datasets.MNIST(root='./data', train=False,
+download=True, transform=transform)
 
-div = sum(sum(filter, []))
-if div == 0:
-    div = 1
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+shuffle=False)
 
-img = Image.open('./foto.jpg')
-pixels = img.load()
+classes = tuple(str(i) for i in range(10))
 
-# Создаем два графика (subplot) для отображения изображений рядом
-plt.subplot(121)
-plt.imshow(img)
+trainloader.dataset.train_data.shape
+testloader.dataset.test_data.shape
 
-img_convolved = img.copy()
-pixels2 = img_convolved.load()
+trainloader.dataset.train_data[0]
 
-start_time=time.time()
+numpy_img = trainloader.dataset.train_data[0].numpy()
+numpy_img.shape
+plt.imshow(numpy_img);
+plt.imshow(numpy_img, cmap='gray');
 
+i = np.random.randint(low=0, high=60000)
+plt.imshow(trainloader.dataset.train_data[i].numpy(), cmap='gray');
 
-for i in range(floor(len(filter)/2), img.height - floor(len(filter)/2)):
-    for j in range(floor(len(filter[0])/2), img.width - floor(len(filter[0])/2)):
-        matrR = []
-        matrG = []
-        matrB = []
-        for n in range(-floor(len(filter)/2), ceil(len(filter)/2)):
-            rowR = []
-            rowG = []
-            rowB = []
-            for m in range(-floor(len(filter[0])/2), ceil(len(filter[0])/2)):
-                px, py = j + m, i + n
-                r, g, b = pixels[px, py] if 0 <= px < img.width and 0 <= py < img.height else (0, 0, 0)
-                rowR.append(r)
-                rowG.append(g)
-                rowB.append(b)
-            matrR.append(rowR)
-            matrG.append(rowG)
-            matrB.append(rowB)
+for data in trainloader:
+    print(len(data))
+    print('Images:',data[0].shape)
+    print('Labels:', data[1].shape)
+    break
 
-        r = np.clip(round(convolution(matrR, filter) / div), 0, 255)
-        g = np.clip(round(convolution(matrG, filter) / div), 0, 255)
-        b = np.clip(round(convolution(matrB, filter) / div), 0, 255)
+class SimpleConvNet(nn.Module):
+    def __init__(self):
+        # вызов конструктора предка
+        super(SimpleConvNet, self).__init__()
+        # необходмо заранее знать, сколько каналов у картинки (сейчас = 1),
+        # которую будем подавать в сеть, больше ничего
+        # про входящие картинки знать не нужно
 
-        pixels2[j, i] = (r, g, b)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
+        self.fc1 = nn.Linear(4 * 4 * 16, 120) # !!!
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        # print(x.shape)
+        x = x.view(-1, 4 * 4 * 16) # !!!
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
-end_time=time.time()-start_time
-st_time1=time.time()
-for i in range(floor(len(filter)/2), img.height - floor(len(filter)/2)):
-    for j in range(floor(len(filter[0])/2), img.width - floor(len(filter[0])/2)):
-        matrR = np.array([[pixels[j+m, i+n][0] if 0 <= j+m < img.width and 0 <= i+n < img.height else 0 for m in range(-floor(len(filter[0])/2), ceil(len(filter[0])/2))]])
-        matrG = np.array([[pixels[j+m, i+n][1] if 0 <= j+m < img.width and 0 <= i+n < img.height else 0 for m in range(-floor(len(filter[0])/2), ceil(len(filter[0])/2))]])
-        matrB = np.array([[pixels[j+m, i+n][2] if 0 <= j+m < img.width and 0 <= i+n < img.height else 0 for m in range(-floor(len(filter[0])/2), ceil(len(filter[0])/2))]])
+net = SimpleConvNet()
+# выбираем функцию потерь
+loss_fn = torch.nn.CrossEntropyLoss()
+# выбираем алгоритм оптимизации и learning_rate
+learning_rate = 1e-4
+optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
+# итерируемся
+for epoch in tqdm(range(3), desc="Epochs"):
 
-        r = np.clip(round(convolve2d(matrR, filter, mode='valid')[0, 0] / div), 0, 255)
-        g = np.clip(round(convolve2d(matrG, filter, mode='valid')[0, 0] / div), 0, 255)
-        b = np.clip(round(convolve2d(matrB, filter, mode='valid')[0, 0] / div), 0, 255)
+    running_loss = 0.0
+    for i, batch in enumerate(tqdm(trainloader)):
+        # так получаем текущий батч
+        X_batch, y_batch = batch
+        # обнуляем веса
+        optimizer.zero_grad()
 
-        pixels2[j, i] = (r, g, b)
+        # forward + backward + optimize
+        y_pred = net(X_batch)
+        loss = loss_fn(y_pred, y_batch)
+        loss.backward()
+        optimizer.step()
+    # выведем текущий loss
+    running_loss += loss.item()
+    # выведем качество каждые 2000 батчей
+    if i % 2000 == 1999:
+        print('[%d, %5d] loss: %.3f' %
+        (epoch + 1, i + 1, running_loss / 2000))
+    running_loss = 0.0
+print('Обучение закончено')
 
-end_time1=time.time()-st_time1
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        y_pred = net(images)
+        _, predicted = torch.max(y_pred, 1)
+        c = (predicted == labels).squeeze()
+        for i in range(4):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+for i in range(10):
+    print('Accuracy of %5s : %2d %%' % (
+        classes[i], 100 * class_correct[i] / class_total[i]))
 
-# Выводим второе изображение (отфильтрованное) на втором графике
-plt.subplot(122)
-plt.imshow(img_convolved)
-plt.show()
+i = np.random.randint(low=0, high=10000)
+def visualize_result(index):
+    image = testloader.dataset.test_data[index].numpy()
+    plt.imshow(image, cmap='gray')
+    y_pred = net(torch.Tensor(image).view(1, 1, 28, 28))
+    _, predicted = torch.max(y_pred, 1)
+    plt.title(f'Predicted: {predicted}')
+    plt.show()
 
-print("Время работы вложенных циклов: ", end_time)
-print("Время работы библиотечных функций: ", end_time1)
+visualize_result(i)
