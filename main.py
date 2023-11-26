c81e9f98c35d30ed0323b2c5a36b56723b4af880
Spfeed <subbart2266@gmail.com>
2023-11-26 11:18:55 +0500

    Лаба 10 фул

--- main.py
+++ main.py
@@ -1,126 +1,90 @@
-import torch
-from torch.nn import Linear, Sigmoid, ReLU
-import pandas as pd
+import pixels2svg
+from PIL import Image
+from math import floor, ceil
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
-N = 100
-D = 2
-K = 3
-X = np.zeros((N * K, D))
-y = np.zeros(N * K, dtype='uint8')
-for j in range(K):
-    ix = range(N * j,N * (j + 1))
-    r = np.linspace(0.0, 1, N)
-    t = np.linspace(j * 4, (j + 1) * 4,N) + np.random.randn(N) * 0.2 # theta
-    X[ix] = np.c_[r * np.sin(t), r * np.cos(t)]
-    y[ix] = j
-
-plt.figure(figsize=(10, 8))
-plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.rainbow)
-plt.title('Спираль', fontsize=15)
-plt.xlabel('$x$', fontsize=14)
-plt.ylabel('$y$', fontsize=14)
-plt.show();
-
-X = torch.autograd.Variable(torch.FloatTensor(X))
-y = torch.autograd.Variable(torch.LongTensor(y.astype(np.int64)))
-
-# N - размер батча (batch_size, нужно для метода оптимизации);
-# D_in - размерность входа (количество признаков у объекта);
-# H - размерность скрытых слоёв;
-# D_out - размерность выходного слоя (суть - количество классов)
-N, D_in, H, D_out = 64, 2, 100, 3
-# Use the nn package to define our model and loss function.
-two_layer_net = torch.nn.Sequential(
-torch.nn.Linear(D_in, H),
-torch.nn.ReLU(),
-torch.nn.Linear(H, D_out),
-)
-loss_fn = torch.nn.CrossEntropyLoss(reduction='none')
-learning_rate = 1e-4
-optimizer = torch.optim.SGD(two_layer_net.parameters(), lr=learning_rate)
-
-for t in range(500):
-    # forward
-    y_pred = two_layer_net(X)
-    # loss
-    loss = loss_fn(y_pred, y).sum()
-    print('{} {}'.format(t, loss.data))
-    # зануляем градиенты (чтобы не было остатка с предыдущего шага)
-    optimizer.zero_grad()
-    # backward
-    loss.backward()
-    # обновляем
-    optimizer.step()
+from scipy.signal import convolve2d
 
 
-h = 0.02
-x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-np.arange(y_min, y_max, h))
-grid_tensor = torch.FloatTensor(np.c_[xx.ravel(), yy.ravel()])
-Z = two_layer_net(torch.autograd.Variable(grid_tensor))
-Z = Z.data.numpy()
-Z = np.argmax(Z, axis=1)
-Z = Z.reshape(xx.shape)
-plt.figure(figsize=(10, 8))
-plt.contourf(xx, yy, Z, cmap=plt.cm.rainbow, alpha=0.3)
-plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.rainbow)
-plt.xlim(xx.min(), xx.max())
-plt.ylim(yy.min(), yy.max())
+def convolution(a, b):
+    s = 0
+    for i in range(len(a)):
+        for j in range(len(a[0])):
+            s += a[i][j] * b[i][j]
+    return s
 
-plt.title('Спираль', fontsize=15)
-plt.xlabel('$x$', fontsize=14)
-plt.ylabel('$y$', fontsize=14)
-plt.show();
+filter = [
+    [-1, -1, 0, 0, 0],
+    [0, -1, -1, -1, 0],
+    [0, -1, 9, -1, 0],
+    [0, -1, -1, -1, 0],
+    [0, 0, 0, 0, 0]
+]
 
-# X = torch.autograd.Variable(torch.FloatTensor(X))
-# y = torch.autograd.Variable(torch.LongTensor(y.astype(np.int64)))
+div = sum(sum(filter, []))
+if div == 0:
+    div = 1
 
-N, D_in, H, D_out = 64, 2, 100, 3
-better_net = torch.nn.Sequential(
-torch.nn.Linear(D_in, H),
-torch.nn.ReLU(),
-torch.nn.Linear(H, H),
-torch.nn.ReLU(),
-torch.nn.Linear(H, H),
-torch.nn.ReLU(),
-torch.nn.Linear(H, D_out),
-)
-loss_fn = torch.nn.CrossEntropyLoss(reduction='none')
-learning_rate = 1e-3
-optimizer = torch.optim.SGD(better_net.parameters(), lr=learning_rate)
-for t in range(1000):
-    # forward
-    y_pred = better_net(X)
-    # loss
-    loss = loss_fn(y_pred, y).sum()
-    print('{} {}'.format(t, loss.data))
-    # зануляем градиенты (чтобы не было остатка с редыдущего шага)
-    optimizer.zero_grad()
-    # backward
-    loss.backward()
-    # обновляем
-    optimizer.step()
+img = Image.open('./foto.jpg')
+pixels = img.load()
 
-h = 0.02
-x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-np.arange(y_min, y_max, h))
-grid_tensor = torch.FloatTensor(np.c_[xx.ravel(), yy.ravel()])
-Z = better_net(torch.autograd.Variable(grid_tensor))
-Z = Z.data.numpy()
-Z = np.argmax(Z, axis=1)
-Z = Z.reshape(xx.shape)
-plt.figure(figsize=(10, 8))
-plt.contourf(xx, yy, Z, cmap=plt.cm.rainbow, alpha=0.3)
-plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.rainbow)
-plt.xlim(xx.min(), xx.max())
-plt.ylim(yy.min(), yy.max())
-plt.title('Спираль', fontsize=15)
-plt.xlabel('$x$', fontsize=14)
-plt.ylabel('$y$', fontsize=14)
-plt.show();
+# Создаем два графика (subplot) для отображения изображений рядом
+plt.subplot(121)
+plt.imshow(img)
+
+img_convolved = img.copy()
+pixels2 = img_convolved.load()
+
+start_time=time.time()
+
+
+for i in range(floor(len(filter)/2), img.height - floor(len(filter)/2)):
+    for j in range(floor(len(filter[0])/2), img.width - floor(len(filter[0])/2)):
+        matrR = []
+        matrG = []
+        matrB = []
+        for n in range(-floor(len(filter)/2), ceil(len(filter)/2)):
+            rowR = []
+            rowG = []
+            rowB = []
+            for m in range(-floor(len(filter[0])/2), ceil(len(filter[0])/2)):
+                px, py = j + m, i + n
+                r, g, b = pixels[px, py] if 0 <= px < img.width and 0 <= py < img.height else (0, 0, 0)
+                rowR.append(r)
+                rowG.append(g)
+                rowB.append(b)
+            matrR.append(rowR)
+            matrG.append(rowG)
+            matrB.append(rowB)
+
+        r = np.clip(round(convolution(matrR, filter) / div), 0, 255)
+        g = np.clip(round(convolution(matrG, filter) / div), 0, 255)
+        b = np.clip(round(convolution(matrB, filter) / div), 0, 255)
+
+        pixels2[j, i] = (r, g, b)
+
+end_time=time.time()-start_time
+st_time1=time.time()
+for i in range(floor(len(filter)/2), img.height - floor(len(filter)/2)):
+    for j in range(floor(len(filter[0])/2), img.width - floor(len(filter[0])/2)):
+        matrR = np.array([[pixels[j+m, i+n][0] if 0 <= j+m < img.width and 0 <= i+n < img.height else 0 for m in range(-floor(len(filter[0])/2), ceil(len(filter[0])/2))]])
+        matrG = np.array([[pixels[j+m, i+n][1] if 0 <= j+m < img.width and 0 <= i+n < img.height else 0 for m in range(-floor(len(filter[0])/2), ceil(len(filter[0])/2))]])
+        matrB = np.array([[pixels[j+m, i+n][2] if 0 <= j+m < img.width and 0 <= i+n < img.height else 0 for m in range(-floor(len(filter[0])/2), ceil(len(filter[0])/2))]])
+
+        r = np.clip(round(convolve2d(matrR, filter, mode='valid')[0, 0] / div), 0, 255)
+        g = np.clip(round(convolve2d(matrG, filter, mode='valid')[0, 0] / div), 0, 255)
+        b = np.clip(round(convolve2d(matrB, filter, mode='valid')[0, 0] / div), 0, 255)
+
+        pixels2[j, i] = (r, g, b)
+
+end_time1=time.time()-st_time1
+
+# Выводим второе изображение (отфильтрованное) на втором графике
+plt.subplot(122)
+plt.imshow(img_convolved)
+plt.show()
+
+print("Время работы вложенных циклов: ", end_time)
+print("Время работы библиотечных функций: ", end_time1)
